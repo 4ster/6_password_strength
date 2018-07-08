c68cec0a1475cf32997d9eba86e768dabeb7d3c5
Andrey Tokarev <abtokarev@gmail.com>
2018-07-08 21:30:27 +0300

    Initial commit

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,12 @@
+import getpass
 
 
 def get_password_strength(password):
-    pass
+    password_strength = 0
+    return password_strength
 
 
 if __name__ == '__main__':
-    pass
+    password = getpass.getpass("Input password: ")
+    print("Password strenght is {0}".format(
+        get_password_strength(password)))
